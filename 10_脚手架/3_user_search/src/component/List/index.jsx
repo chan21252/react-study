704f8a37fe125bc9d73e47de5ede33f61f7dc18d
chan21252 <chanjobs21252@gmail.com>
2022-05-06 16:08:16 +0800

    refactor: 2022-05-06 用户搜索案例 发布/订阅

--- 10_脚手架/3_user_search/src/component/List/index.jsx
+++ 10_脚手架/3_user_search/src/component/List/index.jsx
@@ -1,8 +1,51 @@
 import React, {Component} from 'react';
+import PubSub from 'pubsub-js';
+import axios from "axios";
 
 class List extends Component {
+  state = {
+    userList: [], // 用户列表
+    err: '',      // 错误消息
+    isInitPage: true,  // 是否是初始页面
+    isLoading: false,  // 是否加载中
+  }
+
+  componentDidMount() {
+    // 订阅消息
+    console.log('订阅消息');
+    this.token = PubSub.subscribe('search', this.getUserListData);
+  }
+
+  componentWillUnmount() {
+    console.log('取消订阅消息');
+    PubSub.unsubscribe(this.token);
+  }
+
+  // 获取用户数据
+  getUserListData = (_, keyword) => {
+    console.log(`收到消息---${_}: ${keyword}`);
+
+    if (keyword === '' || typeof keyword === "undefined") {
+      return;
+    }
+
+    this.setState({isLoading: true, isInitPage: false}); // 开始loading
+    axios.get(`https://api.github.com/search/users?q=${keyword}`)
+      .then(
+        (res) => {
+          this.setState({userList: res.data.items, err: ''})
+        },
+        (err) => {
+          this.setState({err: err.message});
+        })
+      .finally(() => {
+        this.setState({isLoading: false});  // 结束loading
+      });
+  }
+
+
   render() {
-    const {userList, err, isInitPage, isLoading} = this.props;
+    const {userList, err, isInitPage, isLoading} = this.state;
     return (
       <div className="row">
         {
@@ -12,7 +55,7 @@ class List extends Component {
             userList.map((user) => {
               return (
                 <div className="card" key={user.id}>
-                  <a href={user.html_url} target="_blank">
+                  <a href={user.html_url} target="_blank" rel="noreferrer">
                     <img alt="avatar" src={user.avatar_url} style={{width: '100px'}}/>
                   </a>
                   <p className="card-text">{user.login}</p>
--- 10_脚手架/3_user_search/src/component/Search/index.jsx
+++ 10_脚手架/3_user_search/src/component/Search/index.jsx
@@ -1,45 +1,25 @@
 import React, {Component} from 'react';
-import axios from "axios";
+import PubSub from "pubsub-js";
 
 class Search extends Component {
 
   // 搜索点击回调
   handleSearch = () => {
-    const {value} = this.searchInput;
-    const {setUserList, updateState, isInitPage} = this.props;
-
+    const {value} = this.searchInputRef;
     if (value === '') {
       return;
     }
-
-    // 改变页面初始状态
-    if (isInitPage) {
-      updateState({isInitPage: false});
-    }
-
-    // 搜索请求
-    updateState({isLoading: true}); // 开始loading
-    axios.get(`https://api.github.com/search/users?q=${value}`)
-      .then(
-        (res) => {
-          setUserList(res.data.items)
-          updateState({err: ''}); //正确返回，清空err
-        },
-        (err) => {
-          updateState({err: err.message});
-        })
-      .finally(() => {
-        updateState({isLoading: false});  // 结束loading
-      });
+    // 发布消息
+    console.log('发布消息---search:', value);
+    PubSub.publish('search', value);
   }
-
 
   render() {
     return (
       <section className="jumbotron">
         <h3 className="jumbotron-heading">搜索Github用户</h3>
         <div>
-          <input type="text" ref={e => this.searchInput = e} placeholder="输入搜索用户名"/>&nbsp;
+          <input type="text" ref={e => this.searchInputRef = e} placeholder="输入搜索用户名"/>&nbsp;
           <button onClick={this.handleSearch}>搜索</button>
         </div>
       </section>
