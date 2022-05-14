71e868ac00d46179bb3707516261434c2f612d8a
chan21252 <chanjobs21252@gmail.com>
2022-05-14 15:16:08 +0800

    feature: 2022-05-14 react-router withRouter

--- 11_react_router/1_demo/src/component/Header/index.jsx
+++ 11_react_router/1_demo/src/component/Header/index.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import {withRouter} from "react-router-dom";
 import './header.css';
 
 class Header extends Component {
@@ -6,9 +7,11 @@ class Header extends Component {
     return (
       <div className="header-bar">
         <h3>Header</h3>
+        <button onClick={this.props.history.goForward} style={{top: '16px', marginLeft:'20px'}} className="btn btn-primary">前进</button>
+        <button onClick={this.props.history.goBack} style={{top: '16px'}} className="btn btn-primary">后退</button>
       </div>
     );
   }
 }
 
-export default Header;
+export default withRouter(Header);
